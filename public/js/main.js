83fcfac5a577a5905cfa15bf8cc9dc48d1970ee6
tzkKrbys <mosohiko@softbank221109065091.bbtec.net>
2015-09-28 00:13:32 +0900

    バックアップ

---
+++ public/js/main.js
@@ -0,0 +1,316 @@
+var canvas;//canvas
+var context;//context
+var mouseX, mouseY;// マウスの位置座標
+var canvasWidth;//canvasのwidth
+var canvasHeight;//canvasのheight
+// キー
+var gBLeftPush = false;	// left
+var gBRightPush = false;	// right
+var gBUpPush = false;		// up
+var gBDownPush = false;	// down
+var myIcon;
+var icons = [];
+
+var socket;
+
+var myUniqueId;
+
+$(document).ready(function(){
+//	function init() {//初期化関数
+		// canvasに代入
+		canvas = document.getElementById('canvas');
+		context = canvas.getContext('2d');
+		canvasWidth = canvas.width;
+		canvasHeight = canvas.height;
+		// canvas非対応
+		if (!canvas || !canvas.getContext) {
+			alert("html5に対応していないので、実行できません");
+			return false;
+		}
+		// キーの登録
+		window.addEventListener('keydown', KeyDown, true); //キーを押した時、呼び出される関数を指定
+		window.addEventListener('keyup', KeyUp, true); //キーを離した時、呼び出される関数を指定
+	
+		//キーを押した時
+		function KeyDown(event) {
+			var code = event.keyCode;       // どのキーが押されたか
+			switch(code) {
+				case 37:// ←キー
+					gBLeftPush = true;
+					break;
+				case 39:// →キー
+					gBRightPush = true;
+					break;
+				case 38:// ↑キー
+					gBUpPush = true;
+					break;
+				case 40:// ↓キー
+					gBDownPush = true;
+					break;
+				case 13:
+					if (event.shiftKey) { // Shiftキーも押された
+						myIcon.SendChat();
+					}
+			}
+		}
+
+		//キーを離した時
+		function KeyUp(event) {
+			code = event.keyCode;
+			switch(code) {
+				case 37:// ←キー
+					gBLeftPush = false;
+					break;
+				case 39:// →キー
+					gBRightPush = false;
+					break;
+				case 38:// ↑キー
+					gBUpPush = false;
+					break;
+				case 40:// ↓キー
+					gBDownPush = false;
+					break;
+			}
+		}
+
+		//エラー処理
+		var errBack = function(e){
+			console.log("Web Audio error:",e.code);
+		};
+		//WebAudioリクエスト成功時に呼び出されるコールバック関数
+		function gotStream(stream){
+			//streamからAudioNodeを作成
+			var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+			mediaStreamSource.connect(filter);
+			filter.connect(analyser);
+			//出力Nodeのdestinationに接続
+			analyser.connect(audioContext.destination);
+			//mediaStreamSource.connect(audioContext.destination);
+		}
+		var audioObj = {"audio":true};
+		//マイクの有無を調べる
+		if(navigator.webkitGetUserMedia){
+			//マイク使って良いか聞いてくる
+			navigator.webkitGetUserMedia(audioObj,gotStream,errBack);
+		}else{
+			console.log("マイクデバイスがありません");
+		}
+//	};
+//	init();
+	socket = io.connect();
+	console.log('connectしました。');
+	// クラス生成
+	myIcon = new MyIcon();		// クラス
+	myIcon.Init( canvasWidth/2, canvasHeight/2 ); //初期化メソッド実行(初期の位置を引数に渡してcanvas要素中央に配置)
+	
+	
+	
+//	$('#myForm').submit(function (e) {
+//		e.preventDefault();
+//		socket.emit('emit_from_client', $('#msg').val());
+//		console.log('emit_from_clientしました');
+//		console.log($('#msg').val());
+//	});
+
+	socket.on('connect', function() {
+//		myUniqueId = socket.id;
+		socket.emit('emit_from_client_join', socket.id);
+		socket.on('emit_from_server_join', function(data) {
+			console.log(data);
+		});
+//		myIcon.uniqueId = myUniqueId;
+//		console.log(myIcon);
+//		socket.emit('client_from_emit_joinIcon', myIcon);//iconをサーバーに送る
+
+		
+//		socket.on('server_from_emit_iconAdd', function (data) {//icons配列を受け取る
+//			console.log(data);
+//			icons = data;
+//			for(var i = 0; i < data.length; i++) {
+//				if(data[i].uniqueId === myUniqueId) {
+//					console.log('発見！' + data[i]);
+//					console.log(data[i]);
+//					console.log(myIcon);
+//				}
+//			}
+//		});
+	});
+//	$("#mkIconBtn").on('click', function (e) {
+//		var data = 'aaa';
+//		socket.emit('emit_from_client_mkIconBtn', data);
+//		console.log(data);
+//	});
+
+//	$('#button').on("click",function(){
+//		myIcon.SendChat();
+//	});
+
+
+//	$(document).on("keydown", function(e) {
+//		if (e.keyCode == 13) { // Enterが押された
+//			$.noop();//何もしないことを明示的に記述
+//			if (e.shiftKey) { // Shiftキーも押された
+//				myIcon.SendChat();
+//			}
+//		} else {
+//			$.noop();//何もしないことを明示的に記述
+//		}
+//	});
+
+
+
+
+
+
+	//canvas要素にイベント設定----------------------s
+	canvas.onmousedown = function () {
+		myIcon.beginDrag();
+	};
+	canvas.onmousemove = function () {
+		mousePos(event);//mouseX,mouseY座標を取得
+		myIcon.drag();
+	};
+	canvas.onmouseup = function () {
+		myIcon.endDrag();
+	};
+
+	//レンダリング関数-----------------------------------------------------
+	window.requestNextAnimationFrame = (function () {
+		var originalWebkitRequestAnimationFrame = undefined,
+			wrapper = undefined,
+			callback = undefined,
+			geckoVersion = 0,
+			userAgent = navigator.userAgent,
+			index = 0,
+			self = this;
+		if (window.webkitRequestAnimationFrame) {
+			wrapper = function (time) {
+				if (time === undefined) {
+					time = +new Date();
+				}
+				self.callback(time);
+			};
+			// Make the switch
+			originalWebkitRequestAnimationFrame = window.webkitRequestAnimationFrame;
+			window.webkitRequestAnimationFrame = function (callback, element) {
+				self.callback = callback;
+				// Browser calls the wrapper and wrapper calls the callback
+				originalWebkitRequestAnimationFrame(wrapper, element);
+			}
+		}
+		if (window.mozRequestAnimationFrame) {
+			index = userAgent.indexOf('rv:');
+			if (userAgent.indexOf('Gecko') != -1) {
+				geckoVersion = userAgent.substr(index + 3, 3);
+				if (geckoVersion === '2.0') {
+					window.mozRequestAnimationFrame = undefined;
+				}
+			}
+		}
+
+		return window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+
+			function (callback, element) {
+			var start,
+				finish;
+
+			window.setTimeout( function () {
+				start = +new Date();
+				callback(start);
+				finish = +new Date();
+				self.timeout = 1000 / 60 - (finish - start);
+			}, self.timeout);
+		};
+	})();
+	//レンダリング関数終了-----------------------------------------------------
+	
+	function animate(now) {//レンダリング関数
+		//符号なし8bitArrayを生成
+		var data = new Uint8Array(analyser.frequencyBinCount);
+		//周波数データ
+		analyser.getByteFrequencyData(data);
+		//console.log(data);
+		var volume = false;
+		for(var i = 0; i < data.length; ++i) {
+			//上部の描画
+			//			context2.fillRect(i*5, 0, 5, data[i]*2);
+			//下部の描画
+			//			context2.fillRect(i*5, h, 5, -data[i]*2);
+			//console.log( data[i] > 100 );
+			if(data[i] > 200){
+				volume = true;
+			}
+		}
+		if( volume ){
+			myIcon.countVoice = 100;
+		}
+		
+		//	Draw
+		//	描画
+		function Draw(){
+			context.fillStyle = "rgb(255,255,255)";// 白に設定。CanvasRenderingContext2Dオブジェクト
+			context.clearRect(0,0,canvasWidth,canvasHeight);// 塗りつぶし。CanvasRenderingContext2Dオブジェクト
+			myIcon.Draw(context,0,0); //myIconオブジェクトの描画メソッド呼出(CanvasRenderingContext2Dオブジェクト,イメージオブジェクト,0,0)
+			myIcon.DrawChat(); //myIconオブジェクトの描画メソッド呼出(CanvasRenderingContext2Dオブジェクト,str)
+			if(myIcon.countVoice){
+				context.globalAlpha = myIcon.countVoice * 3 / 1000;
+				context.fillStyle = "#ff0";
+				context.beginPath();
+				//円の設定（X中心軸,Y中心軸、半径、円のスタート度、円のエンド度、回転）
+				//		context.arc(oldX, oldY, Math.sqrt(Math.pow(px, 2) + Math.pow(py, 2)), 0, Math.PI * 2, false); // full circle
+				context.arc(myIcon.PosX, myIcon.PosY, 140, 0, Math.PI * 2, false); // full circle
+				context.fill();
+				context.globalAlpha = 1;
+				myIcon.countVoice--;
+			}
+		}
+		Draw();		// 描画
+		
+		myIcon.Move(gBRightPush,gBLeftPush,gBUpPush,gBDownPush);//アイコンを動かす
+		requestNextAnimationFrame(animate);//描画がloopする
+	}
+	requestNextAnimationFrame(animate);		// loopスタート
+
+
+	$('.micGainFxEmu').on("click",function(){
+		myIcon.countVoice = 100;
+	});
+
+
+
+
+
+
+
+	function mousePos(event) {// マウスの座標の取得
+		// Canvasの左上のウィンドウ上での座標
+//		var ele = document.getElementById('canvas');
+		var bounds = canvas.getBoundingClientRect();//エレメントの絶対座標値を取得。戻り値はオブジェクトで左の座標値はleft、上の座標値はtopプロパティに入る
+		var offsetX = bounds.left;
+		var offsetY = bounds.top;
+
+		// マウスが押された座標を取得
+		mouseX = event.clientX - offsetX;
+		mouseY = event.clientY - offsetY;
+	}
+
+
+
+
+
+
+
+	var audioContext = new webkitAudioContext();
+	//フィルター
+	var filter = audioContext.createBiquadFilter();
+	filter.type = 0;
+	filter.frequency.value = 440;
+	//analyserオブジェクトの生成
+	var analyser = audioContext.createAnalyser();
+
+
+});
---
+++ server.js
@@ -0,0 +1,255 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+var app = require('./lib/app');
+
+var socketio = require('socket.io');
+
+
+var guestNumber = 1;
+var nickNames = {};
+var namesUsed = [];
+var currentRoom = {};
+
+function send404(response) {
+	response.writeHead(404, {
+		'Content-Type': 'text/plain'
+	});
+	response.write('Error 404: resource not found. ていうか見つかりません');
+	response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(
+		200, {
+			"content-type": mime.lookup(path.basename(filePath))
+		}
+	);
+	response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function (exists) {
+			if (exists) {
+				fs.readFile(absPath, function (err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
+}
+
+var server = http.createServer(function (request, response) {
+	var filePath = false;
+
+	if (request.url == '/') {
+		filePath = 'public/index.html';
+	} else {
+		filePath = 'public' + request.url;
+	}
+
+	var absPath = './' + filePath;
+	serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function () {
+	console.log("Server listening on port 3000.");
+});
+
+
+
+//function listen(server) {
+//	console.log('listen!!!!!!!!!!!!');
+//	io = socketio.listen(server);
+//	io.set('log level', 1);
+//	io.sockets.on('connection', function (socket) {
+//		guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+//		joinRoom(socket, 'Lobby');
+//		handleMessageBroadcasting(socket, nickNames);
+//		handleNameChangeAttempts(socket, nickNames, namesUsed);
+//		handleRoomJoining(socket);
+//		socket.on('rooms', function () {
+//			socket.emit('rooms', io.sockets.manager.rooms);
+//		});
+//		handleClientDisconnection(socket, nickNames, namesUsed);
+//		//追加したやつ
+//		socket.on('emit_from_client', function (data) {
+//			console.log(data);
+//		});
+//	});
+//};
+
+
+
+//var app = require('./lib/app');
+//app.listen(server);
+
+
+
+
+
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+	var name = 'Guest' + guestNumber;
+	nickNames[socket.id] = name;
+	socket.emit('nameResult', {
+		success: true,
+		name: name
+	});
+	namesUsed.push(name);
+	return guestNumber + 1;
+}
+
+function joinRoom(socket, room) {
+	socket.join(room);
+	currentRoom[socket.id] = room;
+	socket.emit('joinResult', {
+		room: room
+	});
+	socket.broadcast.to(room).emit('message', {
+		text: nickNames[socket.id] + ' has joined ' + room + '.'
+	});
+
+	var usersInRoom = io.sockets.clients(room);
+	if (usersInRoom.length > 1) {
+		var usersInRoomSummary = 'Users currently in ' + room + ': ';
+		for (var index in usersInRoom) {
+			var userSocketId = usersInRoom[index].id;
+			if (userSocketId != socket.id) {
+				if (index > 0) {
+					usersInRoomSummary += ', ';
+				}
+				usersInRoomSummary += nickNames[userSocketId];
+			}
+		}
+		usersInRoomSummary += '.';
+		socket.emit('message', {
+			text: usersInRoomSummary
+		});
+	}
+}
+
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+	socket.on('nameAttempt', function (name) {
+		if (name.indexOf('Guest') == 0) {
+			socket.emit('nameResult', {
+				success: false,
+				message: 'Names cannot begin with "Guest".'
+			});
+		} else {
+			if (namesUsed.indexOf(name) == -1) {
+				var previousName = nickNames[socket.id];
+				var previousNameIndex = namesUsed.indexOf(previousName);
+				namesUsed.push(name);
+				nickNames[socket.id] = name;
+				delete namesUsed[previousNameIndex];
+				socket.emit('nameResult', {
+					success: true,
+					name: name
+				});
+				socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+					text: previousName + ' is now known as ' + name + '.'
+				});
+			} else {
+				socket.emit('nameResult', {
+					success: false,
+					message: 'That name is already in use.'
+				});
+			}
+		}
+	});
+}
+
+function handleMessageBroadcasting(socket) {
+	socket.on('message', function (message) {
+		socket.broadcast.to(message.room).emit('message', {
+			text: nickNames[socket.id] + ': ' + message.text
+		});
+	});
+}
+
+function handleRoomJoining(socket) {
+	socket.on('join', function (room) {
+		socket.leave(currentRoom[socket.id]);
+		joinRoom(socket, room.newRoom);
+	});
+}
+
+function handleClientDisconnection(socket) {
+	socket.on('disconnect', function () {
+		var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+		delete namesUsed[nameIndex];
+		delete nickNames[socket.id];
+	});
+}
+
+
+
+
+var io = socketio.listen(server);
+//console.log('こいつはioだぜ。' + io);
+//io.sockets.on()
+var icons = [];
+var ids = [];
+io.sockets.on('connection', function (socket) {
+	console.log('clientから接続がありました');
+	socket.on('emit_from_client', function (data) {
+		console.log(data);
+		socket.emit('emit_from_server', 'you sended message ' + data);
+		io.sockets.emit('emit_from_server', 'you sended message ' + data);
+	});
+	console.log('connection キター！');
+//
+//	socket.on('client_from_emit_icon_move', function(data) {
+//		console.log(data);
+//	});
+	
+	socket.on('client_from_emit_icon_draw', function (data) {
+		io.sockets.emit('server_from_emit_icon_draw', data);
+//		console.log(data);
+	});
+	
+	socket.on('emit_from_client_mkIconBtn', function (data) {
+		console.log(data);
+	});
+//
+//	socket.on('client_from_emit_joinIcon', function(data) {
+//		console.log(data);
+//		icons.push(data);
+//		io.sockets.emit('server_from_emit_iconAdd', icons);
+//	});
+//	socket.on('emit_from_client_join', function (data) {
+//		io.sockets.emit('server_from_emit_iconAdd', data);
+//	});
+	
+	socket.on('emit_from_client_join', function(data) {
+		ids.push(data);
+		console.log(ids);
+		console.log(socket.id);
+		io.sockets.emit('emit_from_server_join', ids);
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
